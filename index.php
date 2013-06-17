249c5d89c3d2d3118b9ae835075f079f0afb2a70
Thomas Hunter <tlhunter@gmail.com>
2013-06-17 18:26:57 -0400

    using predis library

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require 'predis/lib/Predis/Autoloader.php';
+
+Predis\Autoloader::register();
+
+$redis = new Predis\Client();
+$redis->set('foo', 'bar');
+$value = $redis->get('foo');
+
+echo $value;
